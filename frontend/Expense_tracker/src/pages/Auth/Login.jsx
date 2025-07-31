4d4e13ae1f3962f21b284d7a6cd9fbdf1fc9c506
DanielaHG <A00838702@tec.mx>
2025-07-31 17:32:28 -0600

    feat: add login page view

--- frontend/Expense_tracker/src/pages/Auth/Login.jsx
+++ frontend/Expense_tracker/src/pages/Auth/Login.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import AuthLayout from '../../components/layouts/AuthLayout';
 import { useState } from 'react';
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Input from "../../components/Inputs/Input";
+import { validateEmail } from '../../utils/helper';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -10,7 +11,21 @@ const Login = () => {
   const [error, setError] = useState(null);
 
   const handleLogin = async (e) => {
+    e.preventDefault();
 
+    if (!validateEmail(email)) {
+      setError("Please ente a valid email address.");
+      return;
+    }
+
+    if (!password) {
+      setError("Please enter your password.");
+      return;
+    }
+
+    setError("");
+
+    //Login API call
   }
 
   
@@ -31,6 +46,26 @@ const Login = () => {
             placeholder='john@example.com'
             type='text'
           />
+          <Input 
+            value={password}
+            onChange={({target}) => setPassword(target.value)}
+            label="Password"
+            placeholder='********'
+            type='password'
+          />
+
+          {error && <p className='text-red-500 text-xs mt-2'>{error}</p>}
+
+          <button type='submit' className='btn-primary'>
+            LOGIN
+          </button>
+
+          <p className='text-[15px] text-slate-800 mt-3'>
+            Don't have an account? {" "}
+            <Link className='font-medium text-primary underline' to="/signup">
+              Sign up
+            </Link>
+          </p>
         </form>
       </div>
     </AuthLayout>
